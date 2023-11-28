58898efe322949470b9e443c3e6394bdc808ef26
futuraflutter <fluttertrainer.futuralabs@gmail.com>
2023-11-28 12:51:10 +0530

    first commit

---
+++ lib/basic.dart
@@ -0,0 +1,9 @@
+void main() {
+  printDetails(name: "hello", age: 26, place: "kannur");
+}
+
+void printDetails({required name, required place, required age}) {
+  print("name is $name");
+  print("from $place");
+  print(age);
+}
